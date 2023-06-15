4271abfccaa589827a9363aa4cef1df094d2f3ee
aimad kacem <kacem.aimad@gmail.com>
2023-06-15 15:53:04 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the function is printing text
+ *
+ * Description : this program is printing a text message
+ * using the function printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
+
