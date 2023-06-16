5832bbfdbdc2352ff96f73f42a48919279af198a
aimad kacem <kacem.aimad@gmail.com>
2023-06-15 17:00:48 -0700

    modify script sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,7 @@ int main(void)
 	long long int lli_var;
 	float f_var;
 	printf("Size of a char: %lu byte(s)\n", sizeof(c_var));
-	printf("Size of a int: %lu byte(s)\n", sizeof(i_var));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i_var));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(li_var));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli_var));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f_var));
