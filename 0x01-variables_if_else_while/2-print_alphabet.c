4da36045f30da259b5965854a2ae42343b5507fe
aimad kacem <kacem.aimad@gmail.com>
2023-06-16 19:28:40 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,15 +12,8 @@ int main(void)
 	c = 'a';
 	while (c <= 'z')
 	{
-		if (c == 'e' || c == 'q')
-		{
-			c++;
-		}
-		else
-		{
-			putchar(c);
-			c++;
-		}
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);
