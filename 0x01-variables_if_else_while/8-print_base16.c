8ef7994ee6d5b9734159cb7f22152967bec78cd9
aimad kacem <kacem.aimad@gmail.com>
2023-06-16 19:12:49 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print base 16
+ *
+ * description: print alphabets and digits
+ * Return: 0 (true)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(c);
+	}
+	for (n = 98; n < 103; n++)
+        {
+                putchar(c);
+        }
+	putchar('\n');
+	return (0);
+}
