63ff788da68cd9c1b9c1d414e4fac305ccf4eeae
aimad kacem <kacem.aimad@gmail.com>
2023-06-16 19:13:56 -0700

    print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		putchar(c);
+		putchar(n);
 	}
 	for (n = 98; n < 103; n++)
         {
-                putchar(c);
+                putchar(n);
         }
 	putchar('\n');
 	return (0);
