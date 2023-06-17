28bdec1de2afc0ae7c9c24ab7d629dd2c0b6c46b
aimad kacem <kacem.aimad@gmail.com>
2023-06-17 13:50:31 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prin 2 digits
+ *
+ * description: print 2 digits combinations
+ * Return: 0 (true)
+ */
+int main(void)
+{
+	int d1;
+	int d2;
+
+	for (d1 = 48; d1 < 57; d1++)
+	{
+		for (d2 = 49; d2 < 58; d2++)
+		{
+			if (d1 >= d2)
+			{
+			}
+			else
+			{
+				putchar(d1);
+				putchar(d2);
+					if (n != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
