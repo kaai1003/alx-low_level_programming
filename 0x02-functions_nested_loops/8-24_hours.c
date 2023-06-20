8bd8f6be652e1bec95d1875d231efaa7d8ecbd90
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 16:49:47 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - print minutes of the day.
+ * @n: the function accept int argument
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0;i < 25;i++)
+	{
+		for (j = 0;j < 60;j++)
+		{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(':');
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
