6168c34c9fa17a46ec950aaa38f7c705cf214cc4
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 14:10:46 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * main - print alphabets in lower case.
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
