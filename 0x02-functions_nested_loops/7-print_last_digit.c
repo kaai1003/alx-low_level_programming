445e5fd4a7c7674c860d096dc4446e8992c4ebe4
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 16:00:14 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - check lowercase caracter .
+ * @n: the function accept int argument
+ *
+ * Return: n
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
