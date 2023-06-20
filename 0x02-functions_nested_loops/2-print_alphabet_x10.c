2866c4e8dfbcf6f6d442971c225bb7ed66abe11e
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 14:24:55 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lower case alphabets 10x
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alphabet;
+
+	while (i < 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
