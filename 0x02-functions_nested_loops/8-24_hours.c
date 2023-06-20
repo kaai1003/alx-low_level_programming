ad2503e2cb7d735fa3a027e241fe7270a003957a
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 16:55:30 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 	int i;
 	int j;
 
-	for (i = 0;i < 25; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0;j < 60; j++)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar((i / 10) + 48);
 			_putchar((i % 10) + 48);
