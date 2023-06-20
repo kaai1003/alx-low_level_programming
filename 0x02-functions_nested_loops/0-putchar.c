bdb660c6f43d15b834baf34bd78cf4fe170be30d
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 08:55:54 -0700

    readme + 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main function : print _putchar
+ *
+ * this function is display the word "_putchar" using putchar function
+ * return: 0 (success)
+ */
+int main(void)
+{
+	int car = 95;
+	int p = 112;
+	int u = 117;
+	int t = 116;
+	int c = 99;
+	int h = 104;
+	int a = 97;
+	int r = 114;
+	putchar(car);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar('\n');
+	return (0);
+}
