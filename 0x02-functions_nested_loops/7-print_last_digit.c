65315844d6103c1adaa881f3d4f83273ddd622f4
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 16:09:30 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
-	_putchar(n);
-	return (n);
+	int a;	
+
+	if (n < 0)
+	{	
+		n = -n;
+		a = n % 10;
+	}	
+	if (a < 0)
+	{
+		a = -a;
+		_putchar(a + '0');
+	}
+	return (a);
+
 }
