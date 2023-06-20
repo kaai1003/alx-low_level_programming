5f3977b45d531e6f2889a72f5bb75a312c3ea5b7
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 15:55:32 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - check lowercase caracter .
+ * @int: the function accept int argument
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	n = abs(n);
+	return (n);
+}
