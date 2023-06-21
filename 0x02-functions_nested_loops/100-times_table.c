ad77c57737e11a4bcfcc3121a9fc6db27b340186
aimad kacem <kacem.aimad@gmail.com>
2023-06-21 08:35:44 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,28 +22,27 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + 48);
-				}else if (k < 10 && j != 0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + 48);
-				}else if (k >= 10 && k < 100)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + 48);
 					_putchar((k % 10) + 48);
-				}else if (k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + 48);
-					r = k % 100;
-					_putchar((r / 10) + 48);
-					_putchar((r % 10) + 48);
+					_putchar(((k % 100) / 10) + 48);
+					_putchar(((k % 100) % 10) + 48);
 				}
 			}
 			_putchar('\n');
