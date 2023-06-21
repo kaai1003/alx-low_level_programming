e70fa3c16f63eef8d1c54dfd711ffb7452999374
aimad kacem <kacem.aimad@gmail.com>
2023-06-20 17:24:35 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,8 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int i;
+	int j;
 
 	if (n <= 98)
 	{
@@ -21,15 +22,19 @@ void print_to_98(int n)
 			{
 				printf("%d\n", i);
 			}
-		} 
-		else if (n >= 98)
+		}
+	}	
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
 		{
-			for (j = n; j >= 98; j--)
+			if (j != 98)
 			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
+				printf("%d, ", j);
+			}
+			else if (j == 98)
+			{
+				printf("%d\n", j);
 			}
 		}
 	}
