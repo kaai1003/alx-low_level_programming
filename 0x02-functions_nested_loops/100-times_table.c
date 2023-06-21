0dcc234be4d2daa50608caf7822c53458ec27028
aimad kacem <kacem.aimad@gmail.com>
2023-06-21 08:21:26 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 	{
- 	}
+	}
 	else
 	{
 		while (i <= n)
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
- 					_putchar(k + 48);
+					_putchar(k + 48);
 				}
 				else if (k >= 10 && k < 100)
 				{
