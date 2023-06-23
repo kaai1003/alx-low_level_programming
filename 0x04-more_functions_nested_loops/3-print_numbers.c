9f4a65f52fac187468a68a7395f8b6468883fe87
aimad kacem <kacem.aimad@gmail.com>
2023-06-22 18:05:16 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
