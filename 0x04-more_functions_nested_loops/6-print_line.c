fd99f8cbe4800974106500cf69bdc847907f258a
aimad kacem <kacem.aimad@gmail.com>
2023-06-22 19:29:11 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
