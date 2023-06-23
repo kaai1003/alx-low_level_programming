a9f3c53b20fd6bb66484297e1e7779efbaab5609
aimad kacem <kacem.aimad@gmail.com>
2023-06-22 18:49:47 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times numbers from 0 to 14
+ *
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for ( n = 0; n < 15; n++)
+			if ( n < 10 )
+			{
+				_putchar(n + 48);
+			}
+			else
+			{
+				_putchar('1');
+				_putchar(n % 10 + 48);
+			}
+		_putchar('\n');
+	}
+}
