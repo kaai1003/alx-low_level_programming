8bcb0704ff6c6542d67b7619c942baa813e17345
aimad kacem <kacem.aimad@gmail.com>
2023-06-22 19:01:35 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for ( n = 0; n < 15; n++)
+		for (n = 0; n < 15; n++)
 		{
-			if ( n >= 10 )
+			if (n >= 10)
 			{
 				_putchar('1');
-				_putchar(n % 10 + 48);
+				_putchar(n % 10 + '0');
 			}
 		}
 		_putchar('\n');
