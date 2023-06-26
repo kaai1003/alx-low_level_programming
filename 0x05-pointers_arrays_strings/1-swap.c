f9459d25d3caf622c21a8349f14399a0a0b9bcae
aimad kacem <kacem.aimad@gmail.com>
2023-06-26 08:09:24 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swap two int variables
+ * @a: first int variable
+ * @b: second int variable
+ *
+ * Return: nothing to return
+ */
+void swap_int(int *a, int *b)
+{
+	int *mem;
+
+	*mem = *a;
+	*a = *b;
+	*b = *mem;
+}
