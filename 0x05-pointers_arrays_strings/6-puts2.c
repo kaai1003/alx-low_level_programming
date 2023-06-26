c579db67bffc461aad754e615420d6653681331c
aimad kacem <kacem.aimad@gmail.com>
2023-06-26 16:57:00 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints every other caracter of string
+ * @str: string variable
+ *
+ * Return: nothing to return
+ */
+void puts2(char *str)
+{
+	int len;
+	int i;
+
+	len = strlen(str);
+	for (i = 0; i < len-1; i + 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
