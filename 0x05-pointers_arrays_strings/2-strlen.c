b59d0f8a746bbc09e8d9d3abdbe75123c46930a4
aimad kacem <kacem.aimad@gmail.com>
2023-06-26 08:25:39 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen(char *s) - calculate the length of string
+ * @s: string variable
+ *
+ * Return: return the length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	len = strlen(s);
+	return (len);
+}
