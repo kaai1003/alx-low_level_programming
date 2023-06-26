c1677aa47676b91657edec928df5c4ba10534b6a
aimad kacem <kacem.aimad@gmail.com>
2023-06-26 08:01:41 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 -  updates the n pointer value with 98
+ * @*n: parameter variable pointer n
+ *
+ * Return: nothing to return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
