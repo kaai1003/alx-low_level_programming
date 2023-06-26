35fb038fce1046f44aee79d44bc4772edfbaf3ca
aimad kacem <kacem.aimad@gmail.com>
2023-06-26 15:46:29 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - reverse and print string
+ * @s: string to be reversed and printed
+ *
+ * Return: nothing to return
+ */
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
