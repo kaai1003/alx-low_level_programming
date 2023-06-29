0e7dd3e9ae3435aa5cc046da6dcea09e582ebccb
aimad kacem <kacem.aimad@gmail.com>
2023-06-29 16:18:39 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-	{
 		if (src[i] == '\0')
-		{
 			dest[i] = '\0';
-		}
 		else
-		{
 			dest[i] = src[i];
-		}
-	}
-
 	return (dest);
 }
