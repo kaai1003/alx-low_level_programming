40643bf68a43e719dfc3871d92c6a6e95a3b92fd
aimad kacem <kacem.aimad@gmail.com>
2023-06-29 15:34:57 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strca - concatenates two strings
+ * @dest: variable concatenates destination
+ * @src: string to concatenate
+ *
+ * Return: dest variable
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat( dest, src);
+	return (dest);
+}
