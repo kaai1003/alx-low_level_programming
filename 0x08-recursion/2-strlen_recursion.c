32777171d861e98f2c8a75a10b75ad0645b0d97a
aimad kacem <kacem.aimad@gmail.com>
2023-07-05 14:31:17 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print string using recursion
+ * @s: string to print
+ *
+ * Return: return len 
+ */
+void _strlen_recursion(char *s)
+{
+	int len = 1;
+
+	if (*s != '\0')
+	{
+		return(len + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return(0);
+	}
+}
