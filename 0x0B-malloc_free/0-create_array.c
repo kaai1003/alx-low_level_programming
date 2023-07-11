20cf438f192b3dfd0c0b8ecc756b3e0df7edf490
aimad kacem <kacem.aimad@gmail.com>
2023-07-11 14:17:24 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - create array char
+ * @size: size of the array
+ * @c: caractere variable input
+ *
+ * return: NULL if failed , array char otherwise
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr_char;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		arr_char = (char *) malloc(size * sizeof(char));
+		if (arr_char == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				arr_char[i] = c;
+			}
+		}
+	}
+	return (arr_char);
+}
