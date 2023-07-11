70263076b29cff08391caa91e05144cecc221daa
aimad kacem <kacem.aimad@gmail.com>
2023-07-11 16:55:33 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,15 +10,14 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, size;
+	int i, j;
 	int **int_grid;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	size = width * height;
-	int_grid = (int **) malloc(size * sizeof(int *));
+	int_grid = (int **) malloc(height * sizeof(int *));
 	if (int_grid == NULL)
 	{
 		return (NULL);
