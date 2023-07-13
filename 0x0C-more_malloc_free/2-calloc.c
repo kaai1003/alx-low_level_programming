7d45d718ef98ddc547ce77785ab27cb9a261830d
aimad kacem <kacem.aimad@gmail.com>
2023-07-13 10:50:31 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i, s;
 
 	s = nmemb * size;
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
