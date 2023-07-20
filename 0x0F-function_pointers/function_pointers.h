7544744f7c4e9279b61b4567467c410aa485cf8c
aimad kacem <kacem.aimad@gmail.com>
2023-07-19 21:14:32 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <stdlib.h>
 
-typedef __SIZE_TYPE__ size_t;
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
