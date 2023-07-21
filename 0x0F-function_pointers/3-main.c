3d9b20f9fbbf0bb92555db1abd5388e30cb038d9
aimad kacem <kacem.aimad@gmail.com>
2023-07-20 19:50:25 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,24 +8,38 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, result;
-	int (*p)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
-	if (argc < 4 || argc > 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	p = get_op_func(argv[2]);
-	if (p == NULL)
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = p(n1, n2);
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
 	printf("%d\n", result);
+
 	return (0);
 }
