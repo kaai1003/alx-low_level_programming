558d012087c32eb217ace3fe338953f67b93ad44
aimad kacem <kacem.aimad@gmail.com>
2023-08-01 16:13:48 -0700

    free linked list nodes

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - frees linked list
+ * @head: pointer to linked list
+ *
+ * Return: nothing to return
+ */
+void free_listint(listint_t *head);
+{
+	listint_t *node;
+
+	while (head != NULL)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
