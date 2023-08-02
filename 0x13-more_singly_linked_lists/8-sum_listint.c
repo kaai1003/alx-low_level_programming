1c0a2f26c2c4502579c46a371e955eb33654d033
aimad kacem <kacem.aimad@gmail.com>
2023-08-02 09:15:54 -0700

    return sum of linked list elements

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * sum_listint - return sum of all elements on linked list
+ * @head: pointer to linked list
+ *
+ * Return: sum of linked list elements
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+			sum = sum - (head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		return (0);
+	}
+	return (sum);
+}
