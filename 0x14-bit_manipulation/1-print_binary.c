673c5fb17c027f38b0d1ccba49458a554ad41fd3
aimad kacem <kacem.aimad@gmail.com>
2023-08-04 15:12:31 -0700

    print number in binary format

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_binary - print number in binary format
+ * @n: number to convert to binary
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		printf_binary(n >> 1);
+	}
+	_putchar(n & 1) + '0');
+}
