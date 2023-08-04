9052ff4e4a1caa9e6b239b8fa443313c8cb72475
aimad kacem <kacem.aimad@gmail.com>
2023-08-03 19:18:20 -0700

    conversion from binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,57 @@
+#include "main.h"
+/**
+ * get_lenght - get lenght binary number
+ * @b: pointer to strong of chars
+ *
+ * Return: lenght of b or 0
+ */
+int get_lenght(const char *b)
+{
+	int len = 0;
+
+	if (b != NULL)
+	{
+		while (*b)
+		{
+			if (*b != '0' && *b != '1')
+			{
+				len = 0;
+				return (len);
+			}
+			b++;
+			len++;
+		}
+		return (len);
+	}
+	else
+	{
+		len = 0;
+		return (len);
+	}
+}
+/**
+ * binary_to_uint - convert binary number to decimal
+ * @b: pointer to string of chars
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+	int len, base = 1;
+
+	len = get_lenght(b);
+	if (len == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (len -= 1; len >= 0; len--)
+		{
+			dec += (b[len] - 0) * base;
+			base *= 2;
+		}
+		return (dec)
+	}
+}
