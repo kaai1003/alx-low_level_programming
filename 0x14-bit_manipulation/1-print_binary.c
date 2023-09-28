0c5cf4a31494ffdeb059d29efcad2de63e67481f
aimad kacem <kacem.aimad@gmail.com>
2023-09-28 09:34:51 -0700

    prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * number_of_bits - calculate number of bits on given number
+ * @n: number to calculate
+ *
+ * Return: calculated number
+ */
+int number_of_bits(unsigned long int n)
+{
+	int bits = 0;
+	unsigned long int number = n;
+
+	while (number > 0)
+	{
+		bits++;
+		number = number >> 1;
+	}
+	return (bits);
+}
+/**
+ * print_binary - print binary representation of a number
+ * @n: number to print in binary format
+ *
+ * Return: nothing to return
+ */
+void print_binary(unsigned long int n)
+{
+	int bits, i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n == 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		bits = number_of_bits(n);
+		for (i = bits - 1; i >= 0; i--)
+		{
+			if ((n >> i) & 1)
+			{
+				_putchar('1');
+			}
+			else
+			{
+				_putchar('0');
+			}
+		}
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int number_of_bits(unsigned long int n);
 
 #endif
