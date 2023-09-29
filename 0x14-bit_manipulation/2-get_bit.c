0dbdb29fb67e9c19af3a4e3125065d7f6334b3dd
aimad kacem <kacem.aimad@gmail.com>
2023-09-29 01:52:02 -0700

    return the value of bit at given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * number_of_bits - calculate number of bits on given number
+ * @n: number to calculate
+ *
+ * Return: calculated number
+ */
+int number_of_bits(unsigned long int n)
+{
+	int bits = 0;
+	unsigned long int number = n;
+
+	while (number > 0)
+	{
+		bits++;
+		number = number >> 1;
+	}
+	return (bits);
+}
+/**
+ * get_bit - get the value of bit on given index
+ * @n: number
+ * @index: index of bit
+ *
+ * Return: value of bit or -1 on fail
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+	unsigned int n_bits;
+
+	n_bits = number_of_bits(n);
+	if (n_bits <= index)
+	{
+		return (-1);
+	}
+	else
+	{
+			bit = (n >> index) & 1;
+			return (bit);
+	}
+	return (-1);
+}
