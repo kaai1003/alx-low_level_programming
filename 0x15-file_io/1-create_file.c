0d3ab4583251a11e7736274e1a2154a5c5e96b87
aimad kacem <kacem.aimad@gmail.com>
2023-10-02 11:15:45 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - create file and write text on it
+ * @filename: name of the file
+ * @text_content: text ti write on file
+ *
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int count, file, rw_file;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (count = 0; text_content[count];)
+		{
+			count++;
+		}
+	}
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	rw_file = write(file, text_content, count);
+	if (file == -1 || rw_file == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+	return (1);
+}
