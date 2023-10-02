1e1e78c7f1bdb22b4bd0c1dee4e866183820b72c
aimad kacem <kacem.aimad@gmail.com>
2023-10-02 15:09:36 -0700

    modify code to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,19 +14,23 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
 	if (text_content != NULL)
 	{
 		for (count = 0; text_content[count];)
 		{
 			count++;
 		}
-	}
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	rw_file = write(file, text_content, count);
-	if (file == -1 || rw_file == -1)
-	{
-		close(file);
-		return (-1);
+		rw_file = write(file, text_content, count);
+		if (rw_file == -1)
+		{
+			close(file);
+			return (-1);
+		}
 	}
 	close(file);
 	return (1);
