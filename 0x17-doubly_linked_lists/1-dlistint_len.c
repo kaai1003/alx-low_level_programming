76a25f27f7b9e11c608e55388cefe4347fdf34dd
aimad kacem <kacem.aimad@gmail.com>
2023-11-09 06:22:03 -0800

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - return number elements in list
+ * @h: pointer to linked list
+ *
+ * Return: number of elemnts
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t len = 0;
+
+	while (temp !=NULL)
+	{
+		len++;
+		temp = temp->next'
+	}
+	return (len);
+}
