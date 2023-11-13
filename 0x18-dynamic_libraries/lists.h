8a60d59053fcc37134e769707c0995bbb2d6bcaf
aimad kacem <kacem.aimad@gmail.com>
2023-11-13 14:09:28 -0800

    python to c lib

---
+++ 0x18-dynamic_libraries/lists.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+
+int add(int n1, int n2);
+int sub(int n1, int n2);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
---
+++ 0x18-dynamic_libraries/pyfuncs.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+/**
+ * add - add 2 numbers .
+ * @n1: number 1
+ * @n2: number 2
+ *
+ * Return: n1 + n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+/**
+ * sub - sub of 2 numbers .
+ * @n1: number 1
+ * @n2: number 2
+ *
+ * Return: n1 - n2
+ */
+int sub(int n1, int n2)
+{
+	return (n1 - n2);
+}
+/**
+ * mul -  multiplies two integers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div -  division of two integers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod -  modulo of  two integers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
