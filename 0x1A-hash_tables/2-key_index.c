37c3e8d4e1bd81b0aae773cad41683ccb19a7999
AIMAD KACEM <kacem.aimad@gmail.com>
2023-12-22 17:41:37 +0100

    betty style key_index function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,15 +1,14 @@
 #include "hash_tables.h"
 /**
- * key_index - get index of key function
- * @key: pointer to string key
- * @size: size of the table
- * Return: index of the key
+* key_index - get index of key function
+* @key: pointer to string key
+* @size: size of the table
+* Return: index of the key
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index, hash_key;
+	unsigned long int hash_key;
 
 	hash_key = hash_djb2(key);
-	index = hash_key % size;
-	return (index);
+	return (hash_key % size);
 }
