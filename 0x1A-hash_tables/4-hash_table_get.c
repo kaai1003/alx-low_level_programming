4de1df2d28a2d8cd3260fc36ff1b12d5226946f7
AIMAD KACEM <kacem.aimad@gmail.com>
2023-12-23 18:52:06 +0100

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+* hash_table_get - retrieve value associated to key
+* @ht: pointer to hash table
+* @key: pointer to key string
+* Return: pointer to value
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *node;
+
+	index = key_index((unsigned char *) key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+		return (NULL);
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
