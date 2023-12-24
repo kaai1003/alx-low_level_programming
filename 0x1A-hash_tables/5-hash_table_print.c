574108a0e1c1517d74faaeb7782d9aaa3c464b4c
AIMAD KACEM <kacem.aimad@gmail.com>
2023-12-24 15:59:16 +0100

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - print hash table function
+* @ht: pointer to hash table
+* Return: nothing to return
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	bool p = false;
+	hash_node_t *node;
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+	}
+	else
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i] == NULL)
+			{
+				continue;
+			}
+			else if (ht->array[i] != NULL && p == true)
+			{
+				printf(", ");
+			}
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				if (node->next != NULL)
+					printf(", ");
+				p = true;
+				node = node->next;
+			}
+		}
+		printf("}\n");
+	}
+}
